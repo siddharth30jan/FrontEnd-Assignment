3ea90d8110304bf00cbd3aa6353f2a3e36523110
Siddharth Singh <siddharths51700@gmail.com>
2020-10-19 22:43:55 +0530

    Recursive paradigm seems correct!

--- src/Matches.js
+++ src/Matches.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const setDefault = (teamList) => {
-  if (teamList.length == 2) return [];
+  if (teamList.length == 1) return [];
   const arr = new Array(teamList.length / 2);
   arr.fill(null);
   return arr;
@@ -13,32 +13,34 @@ function Matches({ teamList }) {
 
   useEffect(() => {
     //Extract out the winning teams
-    if (teamList.length == 2) return;
+    if (teamList.length == 1) return;
     if (!check()) return;
 
     for (let no = 0; no < teamList.length; no += 2) {
       // if (teamList[no] === null || teamList[no + 1] === null) continue;
       setTimeout(() => {
         // console.log("calledddd");
-        let winner = Math.random() < 0.5 ? teamList[no] : teamList[no + 1];
-        //Extract out the indices pointing to null and then choose a random one from them
-        const possibleCandidates = [];
-        for (let index = 0; index < winningTeams.length; index++) {
-          if (winningTeams[index] === null) possibleCandidates.push(index);
-        }
-        const randomIndex =
-          possibleCandidates[
-            Math.floor(Math.random() * possibleCandidates.length)
-          ];
-        // console.log("ran", randomIndex);
         setWinningTeams((prev) => {
+          let winner = Math.random() < 0.5 ? teamList[no] : teamList[no + 1];
+          //Extract out the indices pointing to null and then choose a random one from them
+          const possibleCandidates = [];
+          for (let index = 0; index < prev.length; index++) {
+            if (prev[index] === null) possibleCandidates.push(index);
+          }
+          //console.log()
+          const randomIndex =
+            possibleCandidates[
+              Math.floor(Math.random() * possibleCandidates.length)
+            ];
+          // console.log("ran", randomIndex);
           let copy = [...prev];
+          //console.log("Wi", winner);
           copy[randomIndex] = winner;
           // console.log("temp", temp);
           return copy;
         });
-        //console.log(Math.floor(Math.random() * 7) + 5);
-      }, 5000);
+        //console.log(Math.floor(Math.random() * 7000) + 5000);
+      }, Math.floor(Math.random() * 7000) + 5000);
     }
   }, [teamList]);
   const check = () => {
@@ -46,10 +48,10 @@ function Matches({ teamList }) {
     return nullArr.length === 0;
   };
   useEffect(() => {
-    if (teamList.length === 2) return;
+    if (teamList.length === 1) return;
     //console.log("Winning Teams", winningTeams);
   }, [winningTeams]);
-  if (teamList.length === 2) return null;
+  if (teamList.length === 1) return null;
   return (
     <div style={{ display: "flex", marginRight: "100px" }}>
       <div>
@@ -57,10 +59,7 @@ function Matches({ teamList }) {
           <p>{team ? team.teamName : "Yet to be decided"}</p>
         ))}
       </div>
-      <div>
-        {console.log("inside", winningTeams)}
-        {teamList.length > 0 && <Matches teamList={winningTeams} />}
-      </div>
+      <div>{teamList.length > 0 && <Matches teamList={winningTeams} />}</div>
     </div>
   );
 }
