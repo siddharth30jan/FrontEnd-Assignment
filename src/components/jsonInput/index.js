4e99deb9ad4996c9adee89d4b49c0f3e7885f84d
Siddharth Singh <siddharths51700@gmail.com>
2020-10-21 20:14:40 +0530

    Added styling to jsonInput component

--- src/components/jsonInput/index.js
+++ src/components/jsonInput/index.js
@@ -1,22 +1,38 @@
 import React from "react";
+import "./styles.css";
+import data from "../../data.json";
 
 function JsonInput({ setTeamlist }) {
   const saveFile = (e) => {
-    var reader = new FileReader();
+    let reader = new FileReader();
 
     reader.onload = function (e) {
-      var jsonObj = JSON.parse(e.target.result);
-      console.log(jsonObj);
+      let jsonObj = JSON.parse(e.target.result);
+      //console.log(jsonObj);
       setTeamlist(jsonObj);
     };
 
     reader.readAsText(e.target.files[0]);
   };
-
+  const setDefault = (e) => {
+    e.preventDefault();
+    setTeamlist(data);
+  };
   return (
-    <div>
-      <h1>Please enter the JSON file</h1>
-      <input type="file" placeholder="Browse" onChange={saveFile} />
+    <div className="outerContainer">
+      <div className="containerJ">
+        <h1>Please enter the JSON</h1>
+        <p>
+          (The program assumes that the JSON file has number of entries in power
+          of 2)
+        </p>
+
+        <input type="file" onChange={saveFile} />
+
+        <button className="button" onClick={setDefault}>
+          Go by the default file
+        </button>
+      </div>
     </div>
   );
 }
